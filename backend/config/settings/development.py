c0bd4f99b51907a44fb02dd6e536c0090743b3b6
ashlapak-cds <anastasiia@cognitive-design-systems.com>
2026-03-08 15:49:45 +0100

    chore: remove railway configuration, update development settings, and clear allowed hosts

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -4,9 +4,11 @@ SECRET_KEY = 'django-insecure-s-%m@-*i9z*xlstvm+nyzeg3-i%74vfws-glk%u#5232%(j74!
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
